3eb2c88f4d5d9428389e8550dd1accc96aea7f05
MINT-905 <112179057+MINT-905@users.noreply.github.com>
2023-06-01 15:39:32 +0800

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageDraw, ImageFont
+from zipfile import ZipFile
+import os
+
+uploaded_file = os.environ['TMPDIR'] + '/' + os.environ['HTTP_FILENAME']
+
+with open(uploaded_file, 'rb') as f:
+    lines = f.read().decode('utf-8').splitlines()
+
+zip_filename = 'images.zip'
+zip_file = ZipFile(zip_filename, 'w')
+
+for index, line in enumerate(lines):
+    image = Image.new('RGB', (100, 100), color='white')
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('arial.ttf', size=14)
+    draw.text((10, 40), line, fill='black', font=font)
+    image_filename = f'image{index}.png'
+    image.save(image_filename)
+    zip_file.write(image_filename)
+
+zip_file.close()
+
+print('Content-Type: application/zip')
+print(f'Content-Disposition: attachment; filename="{zip_filename}"')
+print()
+with open(zip_filename, 'rb') as f:
+    print(f.read())
